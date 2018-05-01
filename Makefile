af8493f0690e88c25db95f7dbb3be2670e03a1e1
shinespark <shinespark64@gmail.com>
2018-05-02 03:19:52 +0900

    前方一致することの無いemojiの最後の:は省略可能に

--- Makefile
+++ Makefile
@@ -4,3 +4,7 @@ init:
 emoji:
 	python3 make_emoji.py > emoji.txt
 	cat dvorakjp_prime.txt emoji.txt > dvorakjp_prime_with_emoji.txt
+
+sorted_emoji:
+	python3 make_emoji.py --sort > emoji.txt
+	cat dvorakjp_prime.txt emoji.txt > dvorakjp_prime_with_emoji.txt
--- make_emoji.py
+++ make_emoji.py
@@ -1,5 +1,11 @@
-import sys
+import argparse
 import requests
+import sys
+from collections import OrderedDict
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-s", "--sort", help="output sorted emojis list", action="store_true")
+args = parser.parse_args()
 
 URL = 'https://raw.githubusercontent.com/emojione/emojione/master/emoji.json'
 r = requests.get(URL)
@@ -8,17 +14,45 @@ if r.status_code != requests.codes.ok:
     print("Couldn't get a response. HTTP Status Code: %s" % r.status_code)
     sys.exit()
 
+pre_d = {}  # { name: emoji }
 for code, v in r.json().items():
-    shortname = v['shortname']
+    shortname = v['shortname'].rstrip(':')
     shortname_alternates = v['shortname_alternates']
     code_points = v['code_points']['base']
 
+    # TODO: 
     # ignore surrogate pairs
     if '-' in code_points:
         continue
 
     emoji = chr(int(code_points, 16))
-    print("%s\t%s" % (shortname, emoji))
+    pre_d[shortname] = emoji
 
+    # only add non forward-matched emojis
     for i in shortname_alternates:
-        print("%s\t%s" % (i, emoji))
+        if not i.startswith(shortname):
+            pre_d[i.rstrip(':')] = emoji
+
+# add colon to prevent unsettled conversion
+d = {}
+for k, v in pre_d.items():
+    is_forward_match = False
+    for key in pre_d.keys():
+        if key == k:  # skip own
+            continue
+        if key.startswith(k):
+            is_forward_match = True
+            break
+
+    if is_forward_match:
+        d[k + ':'] = v
+    else:
+        d[k] = v
+
+# output
+if args.sort:
+    for k, v in OrderedDict(sorted(d.items(), key=lambda t: t[0])).items():
+        print("%s\t%s" % (k, v))
+else:
+    for k, v in d.items():
+        print("%s\t%s" % (k, v))
